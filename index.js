509778f5e4c48bc3f7c439ba307bf2e94ae62193
Tom Medema <tommedema@gmail.com>
2024-04-11 19:24:12 -0700

    cleanup

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const DEFAULT_CHANGE_TIMEOUT = 5000;
 const DEFAULT_WAIT_SETTINGS = {
     timeout: DEFAULT_TIMEOUT
 };
-const SELECTOR_SEND_BUTTON = `document.querySelector("button[data-testid='send-button']")`;
+const SELECTOR_SEND_BUTTON = `button[data-testid='send-button']`;
 const SELECTOR_INPUT = "#prompt-textarea";
 const SELECTOR_ASSISTANT_MESSAGE = 'div[data-message-author-role="assistant"]';
 const awaitInputReady = async (page) => {
